7d890e35a8924f659484179b68b577493a2abe8b
Elijah Potter <117758971+Celestron1123@users.noreply.github.com>
2024-04-18 11:47:13 -0600

    Add files via upload

--- MarkovChain.java
+++ MarkovChain.java
@@ -64,6 +64,54 @@ public class MarkovChain {
 	}
 
 	/**
+	 * A static class to create a HashMap-based Markov Chain. It takes a file and
+	 * processes what words follow a given word. The "following" words are then
+	 * logged in a HashMap as to determine valid word combinations and to determine
+	 * likelyhood of word combinations.
+	 * 
+	 * @param file - The input file
+	 * @return A HashMap Markov Chain
+	 * @throws NoSuchElementException If the file is empty
+	 * @throws FileNotFoundException  If the file name is invalid
+	 */
+	public static HashMap<String, HashMap<String, Integer>> generateOneText(String file)
+			throws NoSuchElementException, FileNotFoundException {
+		HashMap<String, HashMap<String, Integer>> chain = new HashMap<String, HashMap<String, Integer>>();
+		try {
+			Scanner scan = new Scanner(new File(file));
+			String prevWord = null;
+
+			if (scan.hasNext())
+				prevWord = simplify(scan.next());
+
+			else
+				throw new NoSuchElementException();
+
+			while (scan.hasNext()) {
+				String word = simplify(scan.next());
+
+				if (!chain.containsKey(prevWord) && word != null && prevWord != null) {
+					HashMap<String, Integer> map = new HashMap<String, Integer>();
+					map.put(word, 1);
+					chain.put(prevWord, map);
+
+				} else if (word != null && prevWord != null) {
+					HashMap<String, Integer> bruh = chain.get(prevWord);
+					bruh.put(word, bruh.getOrDefault(word, 0) + 1);
+				}
+
+				prevWord = word;
+			}
+			scan.close();
+			return chain;
+		} catch (
+
+		FileNotFoundException e) {
+			throw new FileNotFoundException();
+		}
+	}
+
+	/**
 	 * A private helper class to set a word to lowercase and deliminate based on
 	 * punctuation.
 	 * 
@@ -71,8 +119,8 @@ public class MarkovChain {
 	 * @return The newly formatted word
 	 */
 	private static String simplify(String word) {
-		word = word.replaceAll("[^a-zA-Z ]", "'");
-		String[] stringArr = word.toLowerCase().split("[\\p{Punct}\\s]+");
+		word = word.replaceAll("[^a-zA-Z0-9_ ]", "'");
+		String[] stringArr = word.toLowerCase().split("'");
 		if (stringArr.length == 0)
 			return null;
 		else if (stringArr[0].equals(""))
--- TextGenerator.java
+++ TextGenerator.java
@@ -3,15 +3,22 @@ package comprehensive;
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 
+/**
+ * This class represents the UI for the TextGeneratorModel class. If only three
+ * arguments are passed, the list of most probable words will be returned. If
+ * "all" is passed as a fourth argument then the resultant phrase will be based
+ * on randomness and probability. If "one" is passed then only the most probable
+ * words will be printed in the phrase.
+ */
 public class TextGenerator {
 
 	public static void main(String[] args) throws NoSuchElementException, FileNotFoundException {
 		if (args.length == 3) {
-			System.out.println(TextGeneratorModel.findKMostProbable(args[0], args[1], args[2]));
+			TextGeneratorModel.findKMostProbable(args[0], args[1], args[2]);
 		} else if (args[3].equals("all")) {
-			System.out.println(TextGeneratorModel.createPhrase(args[0], args[1], args[2]));
+			TextGeneratorModel.createPhrase(args[0], args[1], args[2]);
 		} else if (args[3].equals("one")) {
-			System.out.println(TextGeneratorModel.createOnePhrase(args[0], args[1], args[2]));
+			TextGeneratorModel.createOnePhrase(args[0], args[1], args[2]);
 
 		}
 
